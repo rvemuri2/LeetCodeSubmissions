d2bb56d215ae12c41795d40fa9c3b08d8a990ca9
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-08-17 19:56:30 -0700

    Time: 445 ms (24.10%), Space: 16.7 MB (6.93%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -1,36 +1,51 @@
-class Node:
-    def __init__(self, val, prev=None, next=None):
+class ListNode():
+    def __init__(self, val, prev=None, next1=None):
         self.val = val
         self.prev = prev
-        self.next = next
-        
+        self.next = next1
+class BrowserHistory(object):
+
     
-class BrowserHistory:
+    def __init__(self, homepage):
+        self.curr = ListNode(homepage)
+        """
+        :type homepage: str
+        """
+        
 
-    def __init__(self, homepage: str):
+    def visit(self, url):
         
-        self.curr = Node(homepage)
-        
-        
-
-    def visit(self, url: str) -> None:
-        self.curr.next = Node(url, self.curr)
+        self.curr.next = ListNode(url, self.curr)
         self.curr = self.curr.next
         
+        """
+        :type url: str
+        :rtype: None
+        """
+        
 
-    def back(self, steps: int) -> str:
+    def back(self, steps):
+        
         while(self.curr.prev != None and steps > 0):
             self.curr = self.curr.prev
             steps -= 1
-            
+        """
+        :type steps: int
+        :rtype: str
+        """
         return self.curr.val
+        
 
-    def forward(self, steps: int) -> str:
+    def forward(self, steps):
         while(self.curr.next != None and steps > 0):
             self.curr = self.curr.next
             steps -= 1
-            
+        """
+        :type steps: int
+        :rtype: str
+        """
         return self.curr.val
+        
 
 
 # Your BrowserHistory object will be instantiated and called as such:
