091bb8025faa13c90103ef29265e0e83507ad25a
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-08-18 17:10:13 -0700

    Time: 42 ms (70.36%), Space: 16.4 MB (43.81%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -6,8 +6,8 @@ class Solution:
         def GCD(i):
             if(l1 % i and l2 % i):
                 return False   
-            f1, f2 = l1 // i, l2 // i
-            return str2[:i] * f2 == str2 and str2[:i] * f1 == str1
+           
+            return str2[:i] * (l2//i) == str2 and str2[:i] * (l1//i) == str1
                 
         
         for i in range(min(l1, l2), 0, -1):
