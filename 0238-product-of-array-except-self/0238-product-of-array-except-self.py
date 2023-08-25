4cb51d7d5de23eefd663bb480edcd79de850f6d6
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-08-24 17:27:15 -0700

    Time: 163 ms (85.24%), Space: 20.8 MB (73.19%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,16 +1,24 @@
 class Solution(object):
     def productExceptSelf(self, nums):
+        
         arr = [1] * len(nums)
         
         p = 1
-        for i in range(len(nums)):
+        for i in range(len(nums)): 
+            
             arr[i] = p
             p *= nums[i]
             
-        o = 1
-        for i in range(len(nums) - 1, -1, -1):
-            arr[i] *= o
-            o *= nums[i]
+            
+        post = 1
+        
+        for i in range(len(nums)-1, -1, -1):
+            
+            arr[i] *= post
+            post *= nums[i]
             
         return arr
+            
+            
+       
         
