0a753c0f254e9d77b41a900cad327e6d40858591
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-08-25 23:04:33 -0700

    Time: 54 ms (76.98%), Space: 16.9 MB (61.19%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,26 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        return Counter(s) == Counter(t)
+        h1 = {}
+        h2 = {}
+        
+        if(len(s) != len(t)):
+            return False
+        
+        for i in range(len(s)):
+            
+            h1[s[i]] = 1 + h1.get(s[i], 0)
+            h2[t[i]] = 1 + h2.get(t[i], 0)
+            
+        print("h1", h1)
+        print("h2", h2)
+        
+        for j in h1: 
+            
+            if(h1[j] != h2.get(j)):
+                return False
+        
+        return True
         
         
             
