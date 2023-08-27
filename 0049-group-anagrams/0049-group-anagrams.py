05a724118e9c193039a5db4bf0e129a6e0c5bd9b
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-08-26 22:46:51 -0700

    Time: 102 ms (72.54%), Space: 22.2 MB (18.95%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        arr = defaultdict(list)
+        
+        for i in strs:
+            
+            char = [0] * 26 #Character Array
+            
+            for j in i:
+                
+                char[ord(j) - ord("a")] += 1
+                
+                
+            arr[tuple(char)].append(i)
+            
+        return arr.values()
