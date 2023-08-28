41af910624419ff1f3729c0ba95c7c41c07cedfd
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-08-27 23:09:59 -0700

    Time: 81 ms (60.17%), Space: 20.8 MB (29.38%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,24 +1,19 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-    
+class Solution(object):
+    def topKFrequent(self, nums, k):
         h = {}
-        arr = []
+        f = [[] for i in range(len(nums) + 1)]
         
         for i in nums:
+            h[i] = 1 + h.get(i, 0)
             
-            h[i] = 1 + h.get(i, 0) 
+        for i, j in h.items():
+            f[j].append(i)
             
-         
-       
+        r = []
         
-        for i in range(k):
-            val = max(h.values())
-            arr.append((list(h.keys())[list(h.values()).index(val)]))
-            h.pop((list(h.keys())[list(h.values()).index(val)]))
-            
+        for i in range(len(f) - 1, 0, -1):
+            for n in f[i]:
+                r.append(n)
+                if(len(r) == k):
+                    return r
         
-        return arr
-        
-        
-        if(len(nums) == 1):
-            return [nums[0]]
