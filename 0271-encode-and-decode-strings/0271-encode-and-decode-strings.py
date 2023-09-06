d07ee8ca76cacfb6011e839ce77a00712a9b5f11
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-09-05 22:04:07 -0700

    Time: 82 ms (37.10%), Space: 16.9 MB (27.22%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -3,10 +3,8 @@ class Codec:
     def encode(self, strs: List[str]) -> str: 
         str1 = ""
         for i in strs:
-            str1 += str(len(i)) + "!" + i
-            
+            str1 += str(len(i)) + "?" + i
         
-       
         return str1
 
     def decode(self, s: str) -> List[str]:
@@ -15,14 +13,15 @@ class Codec:
         
         while(i < len(s)):
             j = i
-            while(s[j] != "!"):
-                j += 1
-            l = int(s[i:j])
-            arr.append(s[j + 1 : j + 1 + l])
-            i = j + 1 + l
-                
-                
+            while(s[j] != "?"):
+                j+=1
+            
+            length = int(s[i:j])
+            arr.append(s[j+1: j + 1 + length]) 
+            i = j + length + 1
+        
         return arr
+       
                 
         
         """Decodes a single string to a list of strings.
