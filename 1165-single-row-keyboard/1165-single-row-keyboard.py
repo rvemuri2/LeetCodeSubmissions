f5b284fd327f97fc2a8f80b29f87ec1f2979f80e
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-09-24 21:27:34 -0700

    Time: 69 ms (8.40%), Space: 16.5 MB (24.57%) - LeetHub

---
+++ 1165-single-row-keyboard/1165-single-row-keyboard.py
@@ -0,0 +1,23 @@
+class Solution:
+    def calculateTime(self, keyboard: str, word: str) -> int:
+        
+        h = {}
+        count = 0
+        l = 0 
+        c = 0
+        
+        for i in keyboard: 
+            
+            h[i] = c
+            c+=1
+            
+            
+        
+        for i in word: 
+            
+            count += abs((l-h[i]))
+            print(count)
+            l = h[i]
+            
+        return count
+        
