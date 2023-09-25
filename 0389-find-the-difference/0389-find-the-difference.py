496d1fbe8e5dd427019639720bc66375a0dc6de5
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-09-24 22:33:15 -0700

    Time: 38 ms (78.35%), Space: 16.4 MB (8.21%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        
+        
+        c1, c2 = Counter(s), Counter(t)
+        
+        print("c1", c1)
+        print("c2", c2)
+        
+        
+        for i in c1:
+            
+            if i not in c2:
+                return i
+            
+            elif c1[i] < c2[i]:
+                return i
+            
+            
+        for i in c2:
+            if i not in c1:
+               
+                return i
+            
+            elif c2[i] < c1[i]:
+                return i
