97834eba2e9d80aa0349eb5271a825a3fa6da87e
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-09-24 22:07:05 -0700

    Time: 69 ms (70.24%), Space: 16.2 MB (98.40%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,30 +1,20 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        # Determine the maximum number in nums.
-        max_num = max(nums)
         
-        # Create a list to count the occurrences of each number in nums.
-        count = [0] * (max_num + 1)
         
-        # Count the occurrences of each number in nums.
-        for num in nums:
-            count[num] += 1
+       
         
-        # Calculate the number of elements smaller than each number.
-        smaller_counts = [0] * (max_num + 1)
-        smaller_sum = 0
+        arr = sorted(nums)
         
-        for i in range(max_num + 1):
-            smaller_counts[i] = smaller_sum
-            smaller_sum += count[i]
+        arr2 = [1] * len(nums)
         
-        # Create the result array based on the smaller counts.
-        result = [0] * len(nums)
-        for i, num in enumerate(nums):
-            result[i] = smaller_counts[num]
+        for i in range(len(nums)):
+            
+            arr2[i] = arr.index(nums[i])
+            
+            
         
-        return result
-
+        return arr2
             
             
         
