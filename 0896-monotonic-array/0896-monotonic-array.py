8f79a5b4077a46470fdecf5136644ec78e1903f0
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-09-30 00:19:53 -0700

    Time: 922 ms (5.15%), Space: 30.3 MB (98.48%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.py
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -2,27 +2,25 @@ class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
         
         
-        i = 0
-        j = 1
+        
         Lcount = 0
         Scount = 0
-        while(i < len(nums) and j < len(nums)):
+        for i in range(len(nums)-1):
             
-            if(nums[i] > nums[j]):
+            if(nums[i] > nums[i+1]):
                 
                 Scount += 1
             
-            if(nums[i] < nums[j]):
+            if(nums[i] < nums[i+1]):
                 
                 Lcount += 1
         
-            if(nums[i] == nums[j]):
+            if(nums[i] == nums[i+1]):
                 
                 Scount += 1
                 Lcount += 1
                 
-            j+=1
-            i+=1
+            
         
         
         if(Lcount == len(nums)-1 or Scount == len(nums)-1):
