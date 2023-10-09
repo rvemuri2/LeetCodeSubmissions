9742adba67f5a8cda2e9e66109d16b686cd6e250
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-10-08 23:46:47 -0700

    Time: 93 ms (86.26%), Space: 16.3 MB (89.93%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -1,22 +1,15 @@
-class Solution {
-    public int minimumDifference(int[] nums, int k) {
-        //This is a sliding window problem, The intution is we want the min possible difference
-        // So if we sort the array then for the minimum we need to just check the neighbours so used sorting
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+	    if len(nums) <= 1:
+		    return 0
 
-        // Edge case
-        if(k == 1){
-            return 0;
-        }
+	    nums = sorted(nums)
+	    res = nums[k-1] - nums[0]
 
-        Arrays.sort(nums);
-        int min = Integer.MAX_VALUE;
-        int i = 0;
-        int j = k-1;
-        while(j < nums.length){
-            min = Math.min(min, nums[j] - nums[i]); //Finding the min
-            i++;
-            j++;
-        }
-        return min;
-    }
-}
+	    for i in range(k, len(nums)):
+		    res = min(res, nums[i] - nums[i - k + 1])
+
+	    return res
+            
+            
+        
