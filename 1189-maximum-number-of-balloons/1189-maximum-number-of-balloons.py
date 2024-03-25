add01226a1e24c5a1edd8a6ff792d90058782368
Rohit Vemuri <rohitvemuri@yahoo.com>
2024-03-25 12:26:26 -0700

    Time: 44 ms (15.19%), Space: 16.5 MB (91.19%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -12,11 +12,6 @@ class Solution:
         for c in text:
             if c in str1:
                 counter[c] += 1
-        
-        print(counter)
-        for i in str1:
-            
-            h[i] = 1 + h.get(i, 0)
             
         
         return min(counter['b'], counter['a'],counter['l']//2, counter['o']//2, counter['n'])
