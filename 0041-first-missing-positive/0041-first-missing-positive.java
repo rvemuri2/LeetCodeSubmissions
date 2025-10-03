9dceb8948a0d83c946d9ebe84a1be8f1d00fca19
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-10-02 23:41:06 -0700

    Time: 1 ms (100%), Space: 57.7 MB (21.3%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -2,7 +2,6 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
 
-        
         for (int i = 0; i < n; i++) {
             while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
                 int temp = nums[i];
