513f38948c743d353758e8b86e70b7fc219188d0
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-10-02 23:34:55 -0700

    Time: 1 ms (100%), Space: 57.7 MB (21.3%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -12,8 +12,7 @@ class Solution {
             }
         }
         for (int i = 0; i < n; i++) {
-            if (nums[i] != i + 1)
-                return i + 1;
+            if (nums[i] != i + 1) return i + 1;
         }
         return n + 1;
     }
