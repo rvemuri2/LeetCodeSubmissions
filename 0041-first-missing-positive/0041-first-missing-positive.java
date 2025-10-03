98e4734dd86d9cce13fb049308a05dba164fa2c9
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-10-02 23:40:20 -0700

    Time: 1 ms (100%), Space: 57.7 MB (25.29%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -5,7 +5,7 @@ class Solution {
         // Place each number in its right place: nums[i] should be i+1
         for (int i = 0; i < n; i++) {
             while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
-                // Swap nums[i] with nums[nums[i]-1]
+                
                 int temp = nums[i];
                 nums[i] = nums[temp - 1];
                 nums[temp - 1] = temp;
