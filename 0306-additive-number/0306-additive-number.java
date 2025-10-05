885efe17bdedc088a1faf1757e3add48f79ff9de
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-10-04 19:34:43 -0700

    Time: 118 ms (7.89%), Space: 42.2 MB (17.18%) - LeetHub

--- 0306-additive-number/0306-additive-number.java
+++ 0306-additive-number/0306-additive-number.java
@@ -63,7 +63,7 @@ public class Main {
         run("0235813", false, sol);       // cannot start with leading zero
         run("198019823962", true, sol);   // classic additive example
         run("1", false, sol);
-        run("12", false, sol);
+        
     }
 
     private static void run(String num, boolean expected, Solution sol) {
