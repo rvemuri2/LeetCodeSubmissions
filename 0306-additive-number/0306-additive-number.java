a02bd5ccaf10ec5abc6ea92498f301df92d2a2d6
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-10-05 23:55:14 -0700

    Time: 7 ms (8.86%), Space: 42.4 MB (11.45%) - LeetHub

--- 0306-additive-number/0306-additive-number.java
+++ 0306-additive-number/0306-additive-number.java
@@ -48,7 +48,7 @@ class Solution {
 }
 
 public class Main {
-    private static void run(String num, boolean expected, Solution sol) {}
+    
 
     private static boolean isDigits(String s) {
         return true;
