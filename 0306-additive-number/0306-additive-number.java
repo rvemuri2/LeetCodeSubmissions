864c744a76f383ab4c451934883baddb8cf25ff7
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-10-05 23:51:39 -0700

    Time: 5 ms (21.07%), Space: 42.4 MB (14.35%) - LeetHub

--- 0306-additive-number/0306-additive-number.java
+++ 0306-additive-number/0306-additive-number.java
@@ -50,8 +50,7 @@ class Solution {
 public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
-
-                                
+                       
     }
 
     private static void run(String num, boolean expected, Solution sol) {
