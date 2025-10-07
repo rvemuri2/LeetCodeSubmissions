e28468c421d41397cec0fae4f1dfccf153178a48
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-10-06 21:01:12 -0700

    Time: 6 ms (9.3%), Space: 42.2 MB (19.89%) - LeetHub

--- 0306-additive-number/0306-additive-number.java
+++ 0306-additive-number/0306-additive-number.java
@@ -5,7 +5,6 @@ import java.util.List;
 class Solution {
     public boolean isAdditiveNumber(String num) {
         int n = num.length();
-        
         for (int i = 1; i <= n - 2; i++) {
             if (num.charAt(0) == '0' && i > 1) 
                 break;
