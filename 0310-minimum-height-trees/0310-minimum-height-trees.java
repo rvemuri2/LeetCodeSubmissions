a6951693248e665ced3ae0b9ae9de7f5046d4b9c
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-10-07 23:03:08 -0700

    Time: 15 ms (89.6%), Space: 57.7 MB (17.61%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.java
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -52,6 +52,5 @@ public class Main {
         // Line graph (path) -> centers are middle (one or two)
         System.out.println(sol.findMinHeightTrees(5, new int[][]{{0,1},{1,2},{2,3},{3,4}})); // [2]
 
-        
     }
 }
