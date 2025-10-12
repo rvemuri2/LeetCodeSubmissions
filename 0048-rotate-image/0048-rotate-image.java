d4a60734e29138f41ab97763d706c6050848518e
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-10-11 23:53:23 -0700

    Time: 0 ms (100%), Space: 42.2 MB (61.06%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -6,7 +6,7 @@ class Solution {
 
         for(int i = 0;i < n;i++){
             for(int j= i+1;j<n;j++){
-                // swap
+                
                 int temp= matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=temp;
