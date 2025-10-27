4521632c6d15a5d49112f4419130dc5c4bd66c8d
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-10-26 20:54:14 -0700

    Time: 451 ms (13.55%), Space: 83.4 MB (12.2%) - LeetHub

--- 0327-count-of-range-sum/0327-count-of-range-sum.java
+++ 0327-count-of-range-sum/0327-count-of-range-sum.java
@@ -12,6 +12,7 @@ class BST{
         int lo = 0, hi = n-1, res=-1;
         while(hi >= lo){
             int mid=(lo+hi)/2;
+            
             if(arr[mid] >= num) {
                 res=mid;
                 hi=mid-1;
