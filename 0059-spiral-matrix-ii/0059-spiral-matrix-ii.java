8f7028268ab3f02279b88a3c2b4b926258a92738
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-11-01 22:34:50 -0700

    Time: 0 ms (100%), Space: 43.1 MB (8.56%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] ans = new int[n][n];
+        int num = 1, max = n * n;
+        int top = 0, bottom = n - 1, left = 0, right = n - 1;
+
+        while (top <= bottom && left <= right) {
+            // left -> right
+            for (int c = left; c <= right && num <= max; c++) ans[top][c] = num++;
+            top++;
+
+            // top -> bottom
+            for (int r = top; r <= bottom && num <= max; r++) ans[r][right] = num++;
+            right--;
+
+            // right -> left
+            if (top <= bottom) {
+                for (int c = right; c >= left && num <= max; c--) ans[bottom][c] = num++;
+                bottom--;
+            }
+
+            // bottom -> top
+            if (left <= right) {
+                for (int r = bottom; r >= top && num <= max; r--) ans[r][left] = num++;
+                left++;
+            }
+        }
+        return ans;
+    }
+}
