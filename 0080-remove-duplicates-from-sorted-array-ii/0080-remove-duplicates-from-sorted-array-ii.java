8773589dd3df9728936438736f80b128a1890eb6
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-11-04 23:44:23 -0800

    Time: 0 ms (100%), Space: 48.6 MB (5%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -8,12 +8,13 @@ class Solution {
         int k = 2; 
 
         for (int i = 2; i < nums.length; i++) {
+            
             if (nums[i] != nums[k - 2]) {
                 nums[k] = nums[i];
                 k++;
             }
         }
-        
+
         return k;
     }
 }
