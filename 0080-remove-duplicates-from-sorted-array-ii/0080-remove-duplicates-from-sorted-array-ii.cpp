eca6b5459d04cd429252caa39afaeae040b07f48
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-11-05 23:16:52 -0800

    Time: 11 ms (34.68%), Space: 19.7 MB (9.63%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -3,7 +3,6 @@ public:
     int removeDuplicates(vector<int>& nums) {
 
         int i = 0;
-        
         for (auto ele : nums) {
             if(i==0 || i==1 || nums[i-2] != ele)
             {
