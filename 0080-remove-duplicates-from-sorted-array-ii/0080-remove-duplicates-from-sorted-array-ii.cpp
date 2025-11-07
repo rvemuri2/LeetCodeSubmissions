fc2416718eac313861d25b5651ac0d46b1f229fc
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-11-06 23:58:48 -0800

    Time: 4 ms (84.69%), Space: 19.5 MB (56.53%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -4,8 +4,7 @@ public:
 
         int i = 0;
         for (auto ele : nums) {
-            if(i == 0 || i == 1 || nums[i-2] != ele)
-            {
+            if(i == 0 || i == 1 || nums[i-2] != ele) {
                 nums[i] = ele;
                 i++;
             }
