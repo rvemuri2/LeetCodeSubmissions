71ee025c472a0a26388ba46ae2b8e2b6c725c30f
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-11-10 23:37:28 -0800

    Time: 48 ms (9.76%), Space: 48 MB (5.87%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -3,7 +3,6 @@ class Solution {
         int n = nums.length;
         int[] result = new int[2];
         int index = 0;
-
         for (int i = 0; i < n; i++) {
             boolean found = false;
             for (int j = 0; j < n; j++) {
