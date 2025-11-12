a64d0f75fcdfe50e11a23981081ac80d5c8a5339
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-11-11 23:45:10 -0800

    Time: 1 ms (99.88%), Space: 48.1 MB (6.4%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,9 +1,8 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
         int xorAll = 0;
-        for (int num : nums) {
+        for (int num : nums)
             xorAll ^= num;
-        }
 
         int setBit = xorAll & -xorAll;
         int a = 0, b = 0;
