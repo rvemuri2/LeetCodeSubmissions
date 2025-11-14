c9dfd7c911e60537b1b2c09c10eb9c0990fdbde6
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-11-13 23:55:52 -0800

    Time: 2 ms (64.55%), Space: 43.4 MB (13.04%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.java
+++ 0390-elimination-game/0390-elimination-game.java
@@ -1,7 +1,7 @@
 class Solution {
     public int lastRemaining(int n) {
         int head = 1;
-        int step=1;
+        int step = 1;
         boolean left=true;
         while(n>1){
             if(left || n%2==1){
