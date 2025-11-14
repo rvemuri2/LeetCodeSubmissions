1fe02a53b361d8af66a0080d8858446e2f4500f7
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-11-13 23:53:23 -0800

    Time: 2 ms (64.55%), Space: 43.2 MB (19.01%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.java
+++ 0390-elimination-game/0390-elimination-game.java
@@ -1,17 +1,17 @@
 class Solution {
    public int lastRemaining(int n) {
     return helper(n, true);
-}
-
-private int helper(int n, boolean leftToRight) {
-    if (n == 1) {
-        return 1;
     }
 
-    if (leftToRight || n % 2 == 1)
-        return 2 * helper(n / 2, !leftToRight);
+    private int helper(int n, boolean leftToRight) {
+        if (n == 1) {
+            return 1;
+        }   
 
-    else
-        return 2 * helper(n / 2, !leftToRight) - 1;
+        if (leftToRight || n % 2 == 1)
+            return 2 * helper(n / 2, !leftToRight);
+
+        else
+            return 2 * helper(n / 2, !leftToRight) - 1;
     }
 }
