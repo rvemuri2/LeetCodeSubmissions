0dc80ccf7edfcbacca1855dd8e6919395c4b6ce3
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-11-13 23:56:38 -0800

    Time: 2 ms (64.55%), Space: 43.1 MB (19.01%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.java
+++ 0390-elimination-game/0390-elimination-game.java
@@ -3,7 +3,8 @@ class Solution {
         int head = 1;
         int step = 1;
         boolean left = true;
-        while(n>1){
+        
+        while(n > 1) {
             if(left || n%2==1){
                 head+=step;
             }
