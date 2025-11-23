61db301baf8adc0f1a1d4300489fd73d6e712c9a
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-11-22 23:44:23 -0800

    Time: 0 ms (100%), Space: 42 MB (19.58%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -12,11 +12,12 @@ public class Solution {
         if(map.containsKey(n)) { 
             return map.get(n); 
         }
-        
+
         int sum = 0;
         for(int i = 1;i <= n;i++)
             sum += numTrees(i-1, map) * numTrees(n-i, map);
         map.put(n, sum);
+        
         return sum;
     }
 }
