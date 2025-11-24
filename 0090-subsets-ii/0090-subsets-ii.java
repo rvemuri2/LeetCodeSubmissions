6c7767010754b5faa9f2e913e6e0f56bc8949add
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-11-23 22:25:38 -0800

    Time: 3 ms (31.23%), Space: 45.1 MB (13.05%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -9,12 +9,13 @@ class Solution {
 
     private void backtrack(int start, int[] nums, List<Integer> path, List<List<Integer>> list) {
         list.add(new ArrayList<>(path));
+        
         for (int i = start; i < nums.length; i++) {
             if (i > start && nums[i] == nums[i - 1]) {
                 continue;
             }
             path.add(nums[i]);
-            
+
             backtrack(i + 1, nums, path, list);
             path.remove(path.size() - 1);
         }
