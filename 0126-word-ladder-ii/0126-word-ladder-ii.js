62a042297ebc3d4d2c794877434c5eb43ef5cfd5
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-11-24 23:28:04 -0800

    Time: 22 ms (39.77%), Space: 63 MB (18.71%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.js
+++ 0126-word-ladder-ii/0126-word-ladder-ii.js
@@ -2,7 +2,7 @@
  * @param {string} beginWord
  * @param {string} endWord
  * @param {string[]} wordList
- * @return {string[][]}
+
  */
 var findLadders = function(beginWord, endWord, wordList) {
     let connected = (a,b) => {
