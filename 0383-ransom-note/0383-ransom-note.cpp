38946cda26a421a378ee0a72dbbfef2cd8cc7e96
Rohit Vemuri <rohitvemuri@yahoo.com>
2025-12-15 20:04:30 -0800

    Time: 3 ms (56.84%), Space: 11.8 MB (71.52%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool canConstruct(std::string ransomNote, std::string magazine) {
         std::vector<int> alphabet(26, 0);
-
         for (char c : ransomNote) {
             size_t i = magazine.find(c, alphabet[c - 'a']);
             if (i == std::string::npos) {
